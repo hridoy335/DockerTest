ce41dd466b747b5aace60d2507c480e68c557805
Hridoy Roy <roy.hridoy2014work@outlook.com>
2026-09-07 12:03:12 +0600

    update

--- Controllers/Product2Controller.cs
+++ Controllers/Product2Controller.cs
@@ -17,7 +17,6 @@ namespace DockerTestProject.Controllers
         }
 
         // GET: api/product  
-        //update 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
